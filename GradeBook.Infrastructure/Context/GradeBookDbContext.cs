136e8d8764e274a49923957b3a510e55e7c36366
ProgrammingPiotrN <piotr_naw99@outlook.com>
2025-01-10 19:52:22 +0100

    Dodanie połączenia z bazą danych

---
+++ GradeBook.Infrastructure/Context/GradeBookDbContext.cs
@@ -0,0 +1,17 @@
+﻿using GradeBook.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GradeBook.Infrastructure.Context;
+
+internal class GradeBookDbContext : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+    public GradeBookDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema( "gradebook" );
+    }
+}
---
+++ GradeBook.Infrastructure/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using GradeBook.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GradeBook.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<GradeBookDbContext>(ctx => ctx.UseSqlServer(configuration.GetConnectionString("GradeBookCS")));
+
+            return services;
+        }
+    }
+}
